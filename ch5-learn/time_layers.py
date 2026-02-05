8052312ced4f0a8639cd05f9fc320e33c62cddde
Satoshi Tomita <S.Tomita2023@keio.jp>
2026-02-05 21:29:10 +0900

    RNNレイヤの逆伝搬

--- ch5-learn/time_layers.py
+++ ch5-learn/time_layers.py
@@ -1,3 +1,4 @@
+import numpy as np
 class RNN:
     def __init__(self,Wx,Wh,b):
         self.params=[Wx,Wh,b]
@@ -10,3 +11,17 @@ class RNN:
         h_next=np.tanh(t)
         self.cache=(x,h_prev,h_next)
         return h_next
+    
+    def backward(self,dh_next):
+        Wx,Wh,b=self.params
+        x,h_prev,h_next=self.cache
+        dt=dh_next*(1-h_next**2)
+        db=np.sum(dt,axis=0)
+        dWh=np.dot(h_prev.T,dt)
+        dh_prev=np.dot(dt,Wh.T)
+        dWx=np.dot(x.T,dt)
+        dx=np.dot(dt,Wx.T)
+        self.grads[0][...]=dWx
+        self.grads[1][...]=dWh
+        self.grads[2][...]=db
+        return dx,dh_prev
